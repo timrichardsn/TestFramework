3dff80ab6daeb4d4cd3170848a73e4c630306961
Tim Richardson <tim@timrichardson.co>
2019-05-27 20:41:46 +0100

    Initial commit

---
+++ TestFramework/TRUtility.swift
@@ -0,0 +1,16 @@
+//
+//  TRUtility.swift
+//  TestFramework
+//
+//  Created by Tim Richardson on 27/05/2019.
+//  Copyright © 2019 Tim Richardson. All rights reserved.
+//
+
+import Foundation
+
+class TRUtility {
+    
+    static func hello() -> String {
+        return "yo!"
+    }
+}
